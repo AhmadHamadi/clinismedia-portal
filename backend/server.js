b5446bbac8d6e2b17bcc48fca803d24e5fb9efab
AhmadHamadi <117765963+AhmadHamadi@users.noreply.github.com>
2025-07-06 16:40:36 -0400

    little changes

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.get("/", (req, res) => {
   res.send("CliniMedia Portal API is running.");
 });
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
   
